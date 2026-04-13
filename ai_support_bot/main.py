346792aeaafc1f081ca9f469fa8c1ae056a0075b
Mahima Choudhari <Mahimachoudhari88@gmail.com>
2026-04-13 21:09:36 +0530

    Update main.py

--- ai_support_bot/main.py
+++ ai_support_bot/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
-# routers
 from app.api import chat, ingest
 
-app = FastAPI(title="AI Customer Support Bot 🚀")
+app = FastAPI()
 
-# ---------------- CORS (important for UI) ----------------
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,19 +14,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ---------------- ROUTERS ----------------
 app.include_router(chat.router)
 app.include_router(ingest.router)
 
-# ---------------- HOME ----------------
 @app.get("/")
 def home():
-    return {
-        "message": "AI Customer Support Bot Running 🚀",
-        "status": "active"
-    }
+    return {"message": "AI Bot Running"}
 
-# ---------------- UI ROUTE ----------------
 @app.get("/ui")
 def ui():
     return FileResponse("app/index.html")
